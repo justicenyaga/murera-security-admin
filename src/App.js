b09619d259afa0613139e120e16726ea15ff120a
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-18 14:00:01 +0300

    Setup super admin menus

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container } from "@mui/material";
+import {
+  AccountBalance,
+  AdminPanelSettings,
+  Folder,
+  LocalPolice,
+} from "@mui/icons-material";
 
-const App = () => {
-  return <div>App</div>;
-};
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+const menuItems = [
+  {
+    id: 1,
+    label: "Cases",
+    route: "/cases",
+    icon: Folder,
+  },
+  {
+    id: 2,
+    label: "Stations",
+    route: "/stations",
+    icon: AccountBalance,
+  },
+  {
+    id: 3,
+    label: "Officers",
+    route: "/officers",
+    icon: LocalPolice,
+  },
+  {
+    id: 4,
+    label: "Admins",
+    route: "/admins",
+    icon: AdminPanelSettings,
+  },
+];
+
+function App() {
+  const navigate = useNavigate();
+  const userInfo = { isSuperAdmin: true };
+
+  const [selectedMenu, setSelectedMenu] = useState(1);
+
+  const handleMenuItemSelect = (item) => {
+    setSelectedMenu(item.id);
+    navigate(item.route);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <main className="py-3">
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          {userInfo.isSuperAdmin && (
+            <Sidebar
+              menuItems={menuItems}
+              selectedItem={selectedMenu}
+              selectItemHandler={handleMenuItemSelect}
+            />
+          )}
+        </Container>
+      </main>
+    </div>
+  );
+}
 
 export default App;
