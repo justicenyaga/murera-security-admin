ec9d5452c4cb11a0f414f9133a60266c9f10428a
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-19 23:55:42 +0300

    Add index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import AccountPage from "./pages/AccountPage";
