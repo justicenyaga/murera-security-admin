aa6ad889573f3fd2006e8d10e01fba0698732fea
Justice Nyaga <justiceevansmwema@gmail.com>
2024-06-21 10:47:53 +0300

    Implement change email page

--- src/api/users.js
+++ src/api/users.js
@@ -4,6 +4,9 @@ const endpoint = "/users";
 
 const getUser = () => client.get(endpoint + "/me");
 
+const changeEmail = (data) => client.put(endpoint + "/change-email", data);
+
 export default {
+  changeEmail,
   getUser,
 };
--- src/pages/ChangeEmailPage.js
+++ src/pages/ChangeEmailPage.js
@@ -1,7 +1,79 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { LockOutlined, MailOutline } from "@mui/icons-material";
+import * as Yup from "yup";
+
+import { Form, FormField, SubmitButton } from "../components/form";
+
+import usersApi from "../api/users";
+import useApi from "../hooks/useApi";
+import useAuth from "../hooks/useAuth";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("New Email"),
+  password: Yup.string().required().label("Password"),
+});
 
 const ChangeEmailPage = () => {
-  return <div>ChangeEmailPage</div>;
+  const changeEmailApi = useApi(usersApi.changeEmail);
+  const { refreshUser } = useAuth();
+
+  const handleSubmit = async (emailPassword, { resetForm }) => {
+    const { data, ok } = await changeEmailApi.request(emailPassword);
+    if (!ok) return alert(data);
+
+    alert("Email changed successfully");
+    resetForm();
+    refreshUser();
+  };
+
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+      }}
+    >
+      <Typography
+        variant="h6"
+        textAlign="center"
+        width="200px"
+        marginY={3}
+        sx={{ fontSize: 24, fontWeight: 600 }}
+      >
+        Change Email
+      </Typography>
+
+      <Form
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <FormField
+          name="email"
+          Icon={MailOutline}
+          label="New Email"
+          placeholder="Enter new email"
+          required
+        />
+        <FormField
+          name="password"
+          Icon={LockOutlined}
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          required
+        />
+        <SubmitButton
+          title="Change Email"
+          loading={changeEmailApi.loading}
+          fullWidth
+        />
+      </Form>
+    </div>
+  );
 };
 
 export default ChangeEmailPage;
