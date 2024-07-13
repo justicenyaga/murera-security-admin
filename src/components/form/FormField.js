2bb623a356031fe697bcb77fd05a2963e61552b7
Justice Nyaga <justiceevansmwema@gmail.com>
2024-07-13 23:21:41 +0300

    Add optional onChange prop on form field

--- src/components/form/FormField.js
+++ src/components/form/FormField.js
@@ -4,16 +4,21 @@ import PropTypes from "prop-types";
 
 import TextField from "../TextField";
 
-const FormField = ({ name, ...rest }) => {
+const FormField = ({ name, onChange, ...rest }) => {
   const { setFieldTouched, setFieldValue, errors, touched, values } =
     useFormikContext();
 
   const error = touched[name] && errors[name] ? true : false;
 
+  const onChangeHandler = (e) => {
+    setFieldValue(name, e.target.value);
+    if (onChange) onChange(e.target.value);
+  };
+
   return (
     <TextField
       onBlur={() => setFieldTouched(name)}
-      onChange={(e) => setFieldValue(name, e.target.value)}
+      onChange={onChangeHandler}
       error={error}
       helperText={error ? errors[name] : null}
       value={values[name]}
@@ -24,6 +29,7 @@ const FormField = ({ name, ...rest }) => {
 
 FormField.propTypes = {
   name: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
 };
 
 export default FormField;
